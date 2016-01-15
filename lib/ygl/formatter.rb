75d3ae425034f6f7d6f334b9577372f708515c01
s1513114 <s1513114@u.tsukuba.ac.jp>
2016-01-15 10:48:04 +0900

    refactoring formatter module

--- lib/ygl/formatter.rb
+++ lib/ygl/formatter.rb
@@ -4,21 +4,22 @@ module YGL
   module Formatter
     def self.format(format)
       submodules.find do |submodule|
-        format.downcase == module_name(submodule)
+        format.downcase == last_module_name(submodule)
       end
     end
 
     private
 
     def self.submodules
-      consts = constants.collect do |const_name|
-        const_get(const_name)
+      consts = constants.map do |const_name| 
+        const = const_get(const_name)
       end
       consts.select do |const|
         const.class == Module
       end
     end
-    def self.module_name(submodule)
+
+    def self.last_module_name(submodule)
       submodule.name.split('::').last.downcase
     end
   end
