398d32eb8bb15f493d5149933e4c60bcdf801020
s1513114 <s1513114@u.tsukuba.ac.jp>
2016-01-15 13:39:32 +0900

    modify exception in db

--- test/db_test.rb
+++ test/db_test.rb
@@ -1,27 +1,61 @@
 require_relative './test_helper'
 
 class YglDBTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::YGL::VERSION
+  def setup
+    @db = YGL::DB
   end
 
   def test_switch_success
-    assert YGL::DB.switch("daimon-lunch")
+    assert @db.switch("daimon-lunch")
   end
 
   def test_switch_no_such_directroy
-    assert_raises(IOError) { YGL::DB.switch("aaa") }
+    assert_raises(IOError) { @db.switch("aaa") }
   end
 
-  def test_get_toml
-    YGL::DB.switch("daimon-lunch")
+  def test_get_toml_success
+    @db.switch("daimon-lunch")
     result = {"name" => "東麻布 逢坂",
               "url" => "http://tabelog.com/tokyo/A1314/A131401/13044558/"}
-    assert_equal result, YGL::DB.get_toml('oosaka')
+    assert_equal result, @db.get_toml('oosaka')
+  end
+
+  def test_get_toml_no_such_file
+    @db.switch("daimon-lunch")
+    assert_raises(IOError) { @db.switch('aaa') }
+  end
+
+  def test_get_toml_db_does_not_select
+    File.write('config/config.txt', '')
+    assert_raises(ArgumentError) { @db.get_toml('oosaka')}
   end
 
   def test_current_success
-    YGL::DB.switch("daimon-lunch")
-    assert_equal "daimon-lunch", YGL::DB.current
+    @db.switch("daimon-lunch")
+    assert_equal "daimon-lunch", @db.current
+  end
+
+  def test_current_faild
+    File.write('config/config.txt', '')
+    assert_raises(ArgumentError) { @db.current }
+  end
+
+  def test_each
+    @db.switch("daimon-lunch")
+    result = []
+    result[0] = {"name" => "東麻布 逢坂",
+                 "url"  => "http://tabelog.com/tokyo/A1314/A131401/13044558/"}
+    result[1] = {"name" => "蘭麻",
+                 "url"  =>"http://tabelog.com/tokyo/A1314/A131401/13034212/"}   
+    i = 0
+    @db.each do |toml|
+      assert_equal result[i], toml
+      i += 1
+    end
+  end
+
+  def test_each_db_does_not_select
+    File.write('config/config.txt', '')
+    assert_raises(ArgumentError) { @db.each }
   end
 end
