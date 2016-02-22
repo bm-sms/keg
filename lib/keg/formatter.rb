8fdbdc7ffe1fa4e6834344b4792b50c5cdc13f44
s1513114 <s1513114@u.tsukuba.ac.jp>
2016-02-22 15:01:53 +0900

    2016/2/22 作業終了

--- lib/keg/formatter.rb
+++ lib/keg/formatter.rb
@@ -3,15 +3,15 @@ require 'keg'
 module Keg
   class Formatter
     def self.create(format)
-      format = 'json' unless available_format?(format)
+      format = 'json' unless available?(format)
 
-      format_class = const_get(format.capitalize)
-      format_class.new
+      inner_class = const_get(format.capitalize)
+      inner_class.new
     end
 
     private
 
-    def self.available_format?(format)
+    def self.available?(format)
       const_defined?(format.upcase) if alphabet?(format)
     end
 
