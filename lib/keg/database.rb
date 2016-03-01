880caac55b428908d006b4ce733466773ff4fa54
s1513114 <s1513114@u.tsukuba.ac.jp>
2016-03-01 13:39:23 +0900

    refactoring valide process in Database

--- lib/keg/database.rb
+++ lib/keg/database.rb
@@ -5,23 +5,16 @@ module Keg
   class Database
     def initialize(root)
       @root = root
-      @configuration = Configuration.new(root)
+      @configuration = Configuration.new(@root)
     end
 
     def switch(db_name)
-      return if db_name.empty?
-      
-      path = File.join(databases_path, db_name)
-      if Dir.exist?(path)
-        @configuration.save_db_name(db_name)
-      end
+      @configuration.save_db_name(db_name)
     end
 
     def contents(filename)
       path = File.join(current_path, filename+'.toml')
-      if File.exists?(path)
-        TOML.load_file(path)
-      end
+      TOML.load_file(path)
     end
 
     def current
--- test/db_test.rb
+++ test/db_test.rb
@@ -9,21 +9,12 @@ class DBTest < Minitest::Test
   def test_switch_success
     assert @db.switch("glean-daimon-lunch")
   end
-
-  def test_switch_no_such_directroy
-    assert_equal nil, @db.switch("aaa")
-  end
-
+  
   def test_contents_success
     @db.switch("glean-daimon-lunch")
     result = {"name" => "東麻布 逢坂",
               "url" => "http://tabelog.com/tokyo/A1314/A131401/13044558/"}
     assert_equal result, @db.contents('oosaka')
-  end
-
-  def test_contents_no_such_file
-    @db.switch("glean-daimon-lunch")
-    assert_equal nil, @db.contents('aaa')
   end
 
   def test_contents_invalid_config
